210960cac6bdf73465ab82775fc0404cf0d3f595
Priyanshu Dangare <aspizu@protonmail.com>
2025-09-15 05:15:00 +0530

    stuff

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,5 +1,39 @@
 if status is-interactive
     export EDITOR=helix
+    set -U fish_color_autosuggestion brblack
+    set -U fish_color_cancel -r
+    set -U fish_color_command brgreen
+    set -U fish_color_comment brmagenta
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_end brmagenta
+    set -U fish_color_error brred
+    set -U fish_color_escape brcyan
+    set -U fish_color_history_current --bold
+    set -U fish_color_host normal
+    set -U fish_color_match --background=brblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator cyan
+    set -U fish_color_param brblue
+    set -U fish_color_quote yellow
+    set -U fish_color_redirection bryellow
+    set -U fish_color_search_match bryellow '--background=brblack'
+    set -U fish_color_selection white --bold '--background=brblack'
+    set -U fish_color_status red
+    set -U fish_color_user brgreen
+    set -U fish_color_valid_path --underline
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description yellow
+    set -U fish_pager_color_prefix white --bold --underline
+    set -U fish_pager_color_progress brwhite '--background=cyan'
+    alias diff='diff --color=auto'
+    alias grep='grep --color=auto'
+    alias ip='ip -color=auto'
+    export LESS='-R --use-color -Dd+r$Du+b$'
+    export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'"
+    alias ls='eza --icons'
+    alias ll='eza --icons -lh'
+    alias ll='eza --icons -lha'
 end
 
 function fish_greeting
